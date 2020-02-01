14d65480cdcac669ce749f623d74ed468597a3ed
jemy <kndjemy@gmail.com>
2020-02-01 12:57:01 +0300

    latest updated layout

---
+++ README.md
@@ -0,0 +1,2 @@
+# E-commerce-Project
+Source Code FE
--- src/containers/App.js
+++ src/containers/App.js
@@ -11,7 +11,7 @@ function App() {
 		<div>
 			<div className="scrollover">
 			<Navbar />
-
+   {/* for switching screens */}
 				<HomeScreen/>
 			{/* <CartScreen/> */}
 			</div>
