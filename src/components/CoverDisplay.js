82156d06c59a2c2ae1da929b7ba8e93ebb4712db
Tony Jemba <48022053+tonyjemba@users.noreply.github.com>
2021-12-03 23:53:05 +0300

    Update CoverDisplay.js

--- src/components/CoverDisplay.js
+++ src/components/CoverDisplay.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../css/coverdisplay.css';
-import '../media/coverdisplay_media.css'
+import '../media/coverdisplay_media.css';
 
 const CoverDisplay = () => {
     return(
